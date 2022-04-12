ac40c2085d8e8b68e93d533c0aab6057ccd49055
duclotb <101123737+duclotb@users.noreply.github.com>
2022-04-12 16:26:45 +0200

    Add files via upload

---
+++ data/livre.sql
@@ -0,0 +1,17 @@
+CREATE TABLE livre (
+isbn STRING PRIMARY KEY,
+titre STRING,
+complementtitre STRING,
+auteurprincipalnom STRING,
+auteurprincipalprenom STRING,
+autreauteurprincipalnom STRING,
+autreauteurprincipalprenom STRING,
+editeur STRING,
+anneeedition STRING,
+agencedecatalogage STRING,
+typededocument STRING,
+section STRING
+
+);
+
+
