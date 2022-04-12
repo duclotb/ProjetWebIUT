e1570753908c98465e478334bdc8ef11f53e2c10
DUCLOT Bertrand <duclotb@hostname.iut2.upmf-grenoble.fr>
2022-04-12 18:56:46 +0200

    Ajout des pages HTML et CSS de livre (livre.view.php et livre.css)

--- view/livre.view.php
+++ view/livre.view.php
@@ -5,7 +5,41 @@
 ?>
 
 
+<div class = "container livre">
+
+	<h1> Titre du livre </h1><br>
+	<h3> Auteur : XX </h3><br>
+		<div class = "imageachat">
+			<img src="../view/assets/images/imageTest.jpg" alt=""><br>
+		</div>
+	
+		<div class="descriptionarticle">
+			<p> Description de l'article: </p><br>
+		</div>	
+
+	<h2> Prix : 14.50€ <h2><br>
+
+	<button type="button" class="boutonachat">Acheter</button> 
+	<button type="button" class="boutonlisteenvie">Ajouter à la liste d'envie</button> 
+
+</div>	
+
+
+
+
 
 <?php
 	require_once('footer.view.php');
 ?>
+
+
+
+
+
+/* A ajouter : 
+- bouton
+- prix
+- Auteur
+- description du livre
+- bulle d'infor
+*/
