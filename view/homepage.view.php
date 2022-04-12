8dcf0e8e898ff70dc3451269f492a16c8240ae63
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-04-12 15:07:56 +0200

    create html for book cards

--- view/homepage.view.php
+++ view/homepage.view.php
@@ -1,8 +1,24 @@
+<link rel="stylesheet" href="../view/assets/css/homepage.css">
+
 <?php
 	require_once('header.view.php');
 ?>
+	<article>
+		<div>
+			<img src="../view/assets/images/imageTest.jpg" alt="">
+		</div>
+		<div class="information">
+			<div class="foo">
+				<p><strong>Titre du livre</strong></p>
+				<p><i>Editeur</i></p>
+				<p>Auteur</p>
+			</div>
+			<div class="prix">
+				<p>14.50 €</p>
+			</div>
+		</div>
 
-	<h1>Test de balise</h1>
+	</article>
 
 <?php
 	require_once('footer.view.php');
