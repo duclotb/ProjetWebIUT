715f28853adfd22f79237319c6271eb0260f9e79
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-04-12 15:57:12 +0200

    add <a> to article

--- view/homepage.view.php
+++ view/homepage.view.php
@@ -3,22 +3,24 @@
 <?php
 	require_once('header.view.php');
 ?>
-	<article>
-		<div>
-			<img src="../view/assets/images/imageTest.jpg" alt="">
-		</div>
-		<div class="information">
-			<div class="foo">
-				<p><strong>Titre du livre</strong></p>
-				<p><i>Editeur</i></p>
-				<p>Auteur</p>
+<div class="container wrap center">
+	<a href="livre.ctrl.php?id=XXX">
+		<article>
+			<div>
+				<img src="../view/assets/images/imageTest.jpg" alt="">
 			</div>
-			<div class="prix">
-				<p>14.50 €</p>
+			<div class="information">
+				<div class="foo">
+					<p><strong>Titre du livre</strong></p>
+					<p><i>Editeur</i></p>
+					<p>Auteur</p>
+				</div>
+				<div class="prix">
+					<p>14.50 €</p>
+				</div>
 			</div>
-		</div>
-
-	</article>
+		</article>
+	</a>
 
 <?php
 	require_once('footer.view.php');
