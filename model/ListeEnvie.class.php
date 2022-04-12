2a8784bbf9f6bda6180dccde88a8f678f050f359
RACHIDI Camille <rachidic@hostname.iut2.upmf-grenoble.fr>
2022-04-12 18:39:39 +0200

    listeEnvie

---
+++ model/ListeEnvie.class.php
@@ -0,0 +1,29 @@
+<?php
+class listeEnvie() {
+//Attributs
+private $livres = array();
+
+//constructeur
+function __construct(array $livres) {
+  $this->livres = $livres;
+}
+
+//Méthodes
+
+//public
+public function addLivre(Livre $livre) {
+  array_push($this->livres, $livre);
+}
+
+//private
+private function deleteListeEnvie() {
+  unset($this);
+}
+
+
+}
+
+
+
+
+ ?>
