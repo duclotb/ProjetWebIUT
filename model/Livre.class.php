02d3d695700aefbd0c2399ee67743022cbd4ad96
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-04-13 08:27:53 +0200

    add attributes and change the constructor

--- model/Livre.class.php
+++ model/Livre.class.php
@@ -1,80 +1,104 @@
 <?php
 class Livre {
-//Attributs
-private string $titre;
-private $auteurs = array();
-private string $synopsis;
-private float $prix;
-private string $numISBN;
-private $commentaires = array();
+  //Attributs
+  private string $numISBN;
+  private string $titre;
+  private string $sousTitre;
+  private string $auteurNom;
+  private string $auteurPrenom;
+  private string $auteurFacultatifNom;
+  private string $auteurFacultatifPrenom;
+  private string $editeur;
+  private integer $anneeEdition;
+  private string $pages;
+  private string $format;
+  private string $section;
+  private float $prix;
+  private string $synopsis;
+  private $commentaires = array();
 
 
-//Constructeur
-function __construct(string $titre, array $auteurs, string $synopsis, float $prix, string $numISBN, array $commentaires) {
-  $this->titre = $titre;
-  $this->auteurs = $auteurs;
-  $this->synopsis = $synopsis;
-  $this->prix = $prix;
-  $this->numISBN = $numISBN;
-  $this->commentaires = $commentaires;
-}
+  //Constructeur
+  function __construct(string $numISBN, string $titre, string $sousTitre, string $auteurNom, string $auteurPrenom, string $auteurFalcutatifNom, string $auteurFalcutatifPrenom, string $editeur, integer $anneeEdition, string $pages, string $format, string $section, float $prix, string $synopsis, array $commentaires ) {
 
-//Méthodes
+    $this->numISBN = $numISBN;
 
-//public
+    $this->titre = $titre;
+    $this->sousTitre = $sousTitre;
 
+    $this->auteurNom = $auteurNom;
+    $this->auteurPrenom = $auteurPrenom;
 
-public function getTitre() {
-  return $this->titre;
-}
+    $this->auteurFacultatifNom = $auteurFacultatifNom;
+    $this->auteurFacultatifPrenom = $auteurFacultatifPrenom;
 
-public function getAuteurs() {
-  return $this->auteurs;
-}
+    $this->$editeur = $editeur;
+    $this->$anneeEdition = $anneeEdition;
+    $this->$pages = $pages;
+    $this->$format = $format;
+    $this->$section = $section;
+    
+    $this->synopsis = $synopsis;
+    $this->prix = $prix;
+    $this->commentaires = $commentaires;
+  }
 
-public function getSynopsis() {
-  return $this->synopsis;
-}
+  //Méthodes
 
-public function getPrix() {
-  return $this->prix;
-}
-
-public function getNumISBN() {
-  return $this->numISBN;
-}
-
-public function getCommentaires() {
-    return $this->commentaires;
-}
-
-/*public function getInformations() {
-  $array = array();
-  $titre = $this->getTitre();
-  $auteurs = $this->getAuteurs();
-  $synopsis = $this->getSynopsis();
-  $prix = $this->getPrix();
-  $numISBN = $this->getNumISBN();
-  $commentaires = $this->getCommentaires();
-  array_push($array, $titre, $auteurs, $synopsis, $prix, $numISBN, $commentaires);
-  return $array;//retourne un tableau d'informations sur le livre, pour auteurs et commentaires donc un tableau de tableau
-}*/
-
-public function createComment(string $content, Compte $c, Livre $livre) {
-  string $dateCreation = date("mm'/'dd'/'y");
-  $comment = new Commentaire($content, $dateCreation, $c, $livre);
-  $this->addComment();
-}
-
-public function deleteComment(int $id) {
-  unset($this->commentaires[$id]);
-}
+  //public
 
 
-//private
-private function addComment(Commentaire $comment) {
-  array_push($this->commentaires, $comment);
-}
+  public function getTitre() {
+    return $this->titre;
+  }
+
+  public function getAuteurs() {
+    return $this->auteurs;
+  }
+
+  public function getSynopsis() {
+    return $this->synopsis;
+  }
+
+  public function getPrix() {
+    return $this->prix;
+  }
+
+  public function getNumISBN() {
+    return $this->numISBN;
+  }
+
+  public function getCommentaires() {
+      return $this->commentaires;
+  }
+
+  /*public function getInformations() {
+    $array = array();
+    $titre = $this->getTitre();
+    $auteurs = $this->getAuteurs();
+    $synopsis = $this->getSynopsis();
+    $prix = $this->getPrix();
+    $numISBN = $this->getNumISBN();
+    $commentaires = $this->getCommentaires();
+    array_push($array, $titre, $auteurs, $synopsis, $prix, $numISBN, $commentaires);
+    return $array;//retourne un tableau d'informations sur le livre, pour auteurs et commentaires donc un tableau de tableau
+  }*/
+
+  // public function createComment(string $content, Compte $c, Livre $livre) {
+  //   string $dateCreation = date("mm'/'dd'/'y");
+  //   $comment = new Commentaire($content, $dateCreation, $c, $livre);
+  //   $this->addComment();
+  // }
+
+  public function deleteComment(int $id) {
+    unset($this->commentaires[$id]);
+  }
+
+
+  //private
+  private function addComment(Commentaire $comment) {
+    array_push($this->commentaires, $comment);
+  }
 
 
 
