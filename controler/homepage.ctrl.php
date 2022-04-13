18462e6bef2cd2815008d19b4c1fad86ca93758a
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-04-13 15:56:52 +0200

    permet la recherce avec des mots multiples

--- controler/homepage.ctrl.php
+++ controler/homepage.ctrl.php
@@ -6,72 +6,53 @@ require_once('../model/LivreDAO.class.php');
 require_once(__DIR__.'/../framework/view.fw.php');
 
 $keywords = $_GET['keywords'] ?? "";
+$dao = new LivreDAO();
 
 if ($keywords == ""){
-
-	$dao = new LivreDAO();
-
 	$livres = $dao -> getCollectionLivres(0, 100);
-	
-	$isbn = array();
-	$titres = array();
-	$editeurs = array();
-	$auteurs = array();
-	$prix = array();
-	
-	foreach ($livres as $key => $livre) {
-		array_push($isbn, $livre -> getNumISBN());
-		array_push($titres, $livre -> getTitre());
-		array_push($editeurs, $livre -> getEditeur());
-		array_push($auteurs, $livre -> getAuteurs());
-		array_push($prix, $livre -> getPrix());
-	}
-	
-	
-	$view = new View();
-	
-	$view -> assign("isbn", $isbn);
-	$view -> assign("titres", $titres);
-	$view -> assign("editeurs", $editeurs);
-	$view -> assign("auteurs", $auteurs);
-	$view -> assign("prix", $prix);
-	
-	$view -> assign("nombre", count($livres));
-	
-	$view -> display("homepage.view.php");
-	
 } else {
+	$arrKeywords = explode(' ', $keywords);
+	$livres = array();
+	foreach ($arrKeywords as $key => $word) {
 
-	$dao = new LivreDAO();
+		
+		$buffer = $dao -> getLivresKeyword($word);
 
-	$livres = $dao -> getLivresKeywords($keywords);
-	
-	$isbn = array();
-	$titres = array();
-	$editeurs = array();
-	$auteurs = array();
-	$prix = array();
-	
-	foreach ($livres as $key => $livre) {
-		array_push($isbn, $livre -> getNumISBN());
-		array_push($titres, $livre -> getTitre());
-		array_push($editeurs, $livre -> getEditeur());
-		array_push($auteurs, $livre -> getAuteurs());
-		array_push($prix, $livre -> getPrix());
+		
+		foreach ($buffer as $oldISBN => $livre) {
+			$buffer[$buffer[$oldISBN]->getNumISBN()] = $buffer[$oldISBN];
+			unset($buffer[$oldISBN]);
+		}
+
+		$livres = array_merge($livres, $buffer);
 	}
-	
-	
-	$view = new View();
-	
-	$view -> assign("isbn", $isbn);
-	$view -> assign("titres", $titres);
-	$view -> assign("editeurs", $editeurs);
-	$view -> assign("auteurs", $auteurs);
-	$view -> assign("prix", $prix);
-	
-	$view -> assign("nombre", count($livres));
-	
-	$view -> display("homepage.view.php");
 }
+
+$isbn = array();
+$titres = array();
+$editeurs = array();
+$auteurs = array();
+$prix = array();
+
+foreach ($livres as $key => $livre) {
+	array_push($isbn, $livre -> getNumISBN());
+	array_push($titres, $livre -> getTitre());
+	array_push($editeurs, $livre -> getEditeur());
+	array_push($auteurs, $livre -> getAuteurs());
+	array_push($prix, $livre -> getPrix());
+}
+
+
+$view = new View();
+
+$view -> assign("isbn", $isbn);
+$view -> assign("titres", $titres);
+$view -> assign("editeurs", $editeurs);
+$view -> assign("auteurs", $auteurs);
+$view -> assign("prix", $prix);
+
+$view -> assign("nombre", count($livres));
+
+$view -> display("homepage.view.php");
 
 ?>
