4eef797d24b0d1a38a7665a9ebdbcb7b19051b24
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-04-13 08:52:30 +0200

    add dao file and start writting the asign methods

--- controler/main.ctrl.php
+++ controler/main.ctrl.php
@@ -1,9 +1,19 @@
 <?php
 // Inclus le mini framework
+require_once('../model/globalDAO.php');
+require_once('../model/LivreDAO.class.php');
+
 require_once(__DIR__.'/../framework/view.fw.php');
+
+$dao = new LivreDAO();
+
+$livre = $dao -> getTest();
+
+$titre = $livre -> getTitre();
 
 $view = new View();
 
+$view -> assign("titre", $titre);
 $view -> display("homepage.view.php");
 
 ?>
