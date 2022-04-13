f218d37b87a023c2b92d835d2006e8912bf1abbf
RACHIDI Camille <rachidic@hostname.iut2.upmf-grenoble.fr>
2022-04-13 11:47:02 +0200

    sdfsd

--- view/login.view.php
+++ view/login.view.php
@@ -1,19 +1,29 @@
 <link rel="stylesheet" href="../view/assets/css/login.css">
 
-<div class="container login">
+<?php
+	require_once('header.view.php');
+?>
+
+<div class="title">
+
+<h2>LOGIN</h2>
+
+</div>
+
+<div class="container_login">
 
 <form class="" action="index.html" method="post">
 
   <div class="mail">
 
-  <label for="mail">mail:</label><br>
+  <label for="mail">Mail:</label><br>
   <input type="text" name="mail" id="mail">
 
   </div>
 
   <div class="mdp">
 
-  <label for="mdp">mot de passe:</label><br>
+  <label for="mdp">Mot de passe:</label><br>
   <input type="text" name="mdp" id="mdp">
 
   </div>
@@ -27,3 +37,7 @@
 </form>
 
 </div>
+
+<?php
+	require_once('footer.view.php');
+?>
