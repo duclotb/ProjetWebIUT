857387f5749cb613431bcab61ca71f739f909420
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-04-13 11:01:14 +0200

    test file

--- model/test/Livre.test.php
+++ model/test/Livre.test.php
@@ -6,14 +6,18 @@ $auteurs = array('Stendhal','Charlemagne');
 $synopsis = "Un homme qui tombe amoureux d'une femme";
 $commentaires = array("c'est génial", "cest nul");
 
-$livre = new Livre('le Rouge et le Noir', $auteurs, $synopsis,3.4,'54',$commentaires);
+// $livre = new Livre('le Rouge et le Noir', $auteurs, $synopsis,3.4,'54',$commentaires);
 
-foreach ($livre->getAuteurs() as $key => $value) {
-  print($value);
-}
+// foreach ($livre->getAuteurs() as $key => $value) {
+//   print($value);
+// }
 
-foreach ($livre->getInformations() as $key => $value) {
-  print($value);
-}
+// foreach ($livre->getInformations() as $key => $value) {
+//   print($value);
+// }
+
+$livre = new Livre("t", "t", "t", "t", "t", "t", "t", "t", 200, "t", "t", "t", 1.5, "t", "t");
+print_r($livre);
+var_dump($livre);
 
  ?>
--- model/test/LivreDAO.test.php
+++ model/test/LivreDAO.test.php
@@ -6,13 +6,25 @@
 	$dao = new LivreDAO();
 
 	$livre = $dao -> getTest();
-	// print_r($livre);
+	print_r($livre);
 
 	$titre = $livre -> getTitre();
 
 	$livres = $dao -> getCollectionLivres(1,2);
 
-	print_r($livres);
+	$titres = array();
+	$editeurs = array();
+	$auteurs = array();
+	$prix = array();
+	
+	foreach ($livres as $key => $livre) {
+		array_push($titres, $livre -> getTitre());
+		array_push($editeurs, $livre -> getEditeur());
+		array_push($auteurs, $livre -> getAuteurs());
+		array_push($prix, $livre -> getPrix());
+	}
+
+	print_r($auteurs);
 
 
 ?>
