0e66f14d832762158103d92f96927373afbc9dae
RACHIDI Camille <rachidic@hostname.iut2.upmf-grenoble.fr>
2022-04-13 10:08:01 +0200

    login view

---
+++ view/login.view.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="../view/assets/css/login.css">
+
+<div class="container login">
+
+<form class="" action="index.html" method="post">
+
+  <div class="mail">
+
+  <label for="mail">mail:</label><br>
+  <input type="text" name="mail" id="mail">
+
+  </div>
+
+  <div class="mdp">
+
+  <label for="mdp">mot de passe:</label><br>
+  <input type="text" name="mdp" id="mdp">
+
+  </div>
+
+<div class="submit_button">
+
+  <input type="submit" value="login">
+
+  </div>
+
+</form>
+
+</div>
