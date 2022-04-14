f955efecd21f41ea0a0792920ab2b04a00fc3950
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-04-14 18:44:35 +0200

    update footer

--- view/footer.view.php
+++ view/footer.view.php
@@ -4,30 +4,18 @@
 <footer>
 	<div class="container space-between">
 		<div class="contact">
-			<p><strong>mail : </strong> </p>
 			<ul>
-				<li>atlas@gmail.com</li>
+				<li><strong>mail : </strong>atlas@gmail.com</li>
+				<li><strong>adresse : </strong>75, rue du livre 38000 Grenoble</li>
+				<li><strong>teléphone : </strong>+33 (0)6 23 85 47 95</li>
 			</ul>
-			<p><strong>adresse :</strong></p>
-			<ul>
-				<li> 75, rue du livre 38000 Grenoble</li>
-			</ul>
-			<p><strong>phone : </strong></p>
-			<ul>
-				<li>+33 (0)6 23 85 47 95</li>
-			</ul>
+
 		</div>
 		<div class="countries">
-			<ul>
-				<li>France</li>
-				<li>Germany</li>
-				<li>UK</li>
-				<li>Japan</li>
-			</ul>
+			<p>Disponible en <strong>France</strong> • <strong>Allemagne</strong> • <strong>Grande-Bretagne</strong> • <strong>Japon</strong></p>
 		</div>
 		<div class="paiement">
-			<ul>
-				<li>MOYENS DE PAIEMENT</li>
+			<ul><strong>Moyens de paiement</strong>
 				<li>Visa</li>
 				<li>Mastercard</li>
 				<li>Virement</li>
