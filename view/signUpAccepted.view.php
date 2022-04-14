4cc9b97a8fd661fbff1c6e7a1551217297025bc9
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-04-14 11:45:39 +0200

    view for signup

---
+++ view/signUpAccepted.view.php
@@ -0,0 +1,14 @@
+
+<?php
+	require_once('header.view.php');
+?>
+
+<h1>Récapitulatif</h1>
+<p>Nom : <?=$nom?></p>
+<p>Prenom : <?=$prenom?></p>
+<p>Date de Naissance : <?=$dateNaissance?></p>
+<p>Mail : <?=$mail?></p>
+
+<?php
+	require_once('footer.view.php');
+?>
---
+++ view/signUpRejected.view.php
@@ -0,0 +1,10 @@
+
+<?php
+	require_once('header.view.php');
+?>
+
+<h1><?=$message?></h1>
+
+<?php
+	require_once('footer.view.php');
+?>
