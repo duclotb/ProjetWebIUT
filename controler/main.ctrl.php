638d113333161c1b1fd250fb71f3ba118886eb23
RACHIDI Camille <rachidic@hostname.iut2.upmf-grenoble.fr>
2022-04-14 14:32:11 +0200

    wxc

---
+++ controler/main.ctrl.php
@@ -0,0 +1,39 @@
+<?php
+// Inclus le mini framework
+require_once('../model/globalDAO.php');
+require_once('../model/LivreDAO.class.php');
+
+require_once(__DIR__.'/../framework/view.fw.php');
+
+$dao = new LivreDAO();
+
+$livres = $dao -> getCollectionLivres(0, 100);
+
+$isbn = array();
+$titres = array();
+$editeurs = array();
+$auteurs = array();
+$prix = array();
+
+foreach ($livres as $key => $livre) {
+	array_push($isbn, $livre -> getNumISBN());
+	array_push($titres, $livre -> getTitre());
+	array_push($editeurs, $livre -> getEditeur());
+	array_push($auteurs, $livre -> getAuteurs());
+	array_push($prix, $livre -> getPrix());
+}
+
+
+$view = new View();
+
+$view -> assign("isbn", $isbn);
+$view -> assign("titres", $titres);
+$view -> assign("editeurs", $editeurs);
+$view -> assign("auteurs", $auteurs);
+$view -> assign("prix", $prix);
+
+$view -> assign("nombre", count($livres));
+
+$view -> display("homepage.view.php");
+
+?>
