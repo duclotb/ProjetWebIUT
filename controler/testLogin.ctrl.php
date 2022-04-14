b41b09a2bacaff1595a9dc05c8ec0fc8fdd72637
DUCLOT Bertrand <duclotb@hostname.iut2.upmf-grenoble.fr>
2022-04-14 16:34:09 +0200

    message

---
+++ controler/testLogin.ctrl.php
@@ -0,0 +1,31 @@
+<?php
+// Inclut le mini framework
+require_once(__DIR__.'/../framework/view.fw.php');
+require_once('../model/globalDAO.php');
+require_once('../model/CompteDAO.class.php');
+
+// Si on essaie de se connecter et que le compte n'existe pas => renvoyer vers une page avec message d'erreur, sinon vers page compte
+
+$mail = $_POST["mail"] ?? "";
+
+$dao = new CompteDAO();
+
+$compte = $dao -> getCompte($mail);
+
+$view = new View();
+
+// Si le compte n'existe pas, afficher
+if (!$compte){
+    $view ->assign("messagerefusconnexion", "Ce compte n'existe pas");
+    $view ->display("login.view.php");
+}
+
+else {
+    $view ->display("homepage.view.php");
+
+    
+
+}
+
+
+?>
