323d18974ff329c67608c2f2028359ee951cdba9
RACHIDI Camille <rachidic@hostname.iut2.upmf-grenoble.fr>
2022-04-14 16:13:45 +0200

    qsd

--- view/footer.view.php
+++ view/footer.view.php
@@ -1,9 +1,40 @@
+<link rel="stylesheet" href="../view/assets/css/footer.css">
+
 </main>
 </div>
 
 <footer>
-	<div class="container">
-		<h2>ceci est le footer</h2>
+	<div class="container space-between">
+		<div class="contact">
+			<p><strong>mail : </strong> </p>
+			<ul>
+				<li>atlas@gmail.com</li>
+			</ul>
+			<p><strong>adresse :</strong></p>
+			<ul>
+				<li> 75, rue du livre 38000 Grenoble</li>
+			</ul>
+			<p><strong>phone : </strong></p>
+			<ul>
+				<li>+33 (0)6 23 85 47 95</li>
+			</ul>
+		</div>
+		<div class="countries">
+			<ul>
+				<li>France</li>
+				<li>Germany</li>
+				<li>UK</li>
+				<li>Japan</li>
+			</ul>
+		</div>
+		<div class="paiement">
+			<ul>
+				<li>MOYENS DE PAIEMENT</li>
+				<li>Visa</li>
+				<li>Mastercard</li>
+				<li>Virement</li>
+			</ul>
+	</div>
 	</div>
 </footer>
 </body>
