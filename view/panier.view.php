fe656a9e4c0434bc80aecd6c7924df492d681586
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-04-15 11:40:15 +0200

    add direction to book on click image

--- view/panier.view.php
+++ view/panier.view.php
@@ -6,8 +6,8 @@
     <div>
       <?php for ($i = 0; $i < $nombre; $i++) :?>
 
-      <div class="rows"> 
-        <img src="<?=$imageURLs[$i]?>" alt="">
+      <div class="rows">
+        <a href="livre.ctrl.php?isbn=<?=htmlentities(urlencode("$isbn[$i]"))?>"><img src="<?=$imageURLs[$i]?>" alt=""></a>
         <div class="info">
           <p><strong>Titre</strong> : <?=$titres[$i]?></p>
           <p><strong>Prix</strong> : <?=$prix[$i]?> €</p>
@@ -18,7 +18,10 @@
     </div>
 
     <div>
-      <p id="total"><strong>Prix Total</strong> : <?=$prixTotal?> €</p>
+      <p id="total">
+        <strong>Total article</strong> : <?=$nombreArticle?>
+        <br>
+        <strong>Prix Total</strong> : <?=$prixTotal?> €</p>
       <a href="redirection.ctrl.php" class="btn">Commander</a>
     </div>
 
